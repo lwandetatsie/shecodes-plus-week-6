499acd3f959611f347098493df7577ec168a5c7d
Lwande <lwandemdaka@gmail.com>
2024-02-01 19:53:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -88,15 +88,9 @@ function displayForecast(response) {
       `
       <div class="weather-forecast">
       <div class="weather-forecast-date">${formatDay(day.time)}</div>
-      </div>
-      <div class="weather-forecast">
       <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
-      </div>
-      <div class="weather-forecast">
       <div class="weather-forecast-temp">
       <span class="weather-forecast-temp-max">${Math.round(day.temperature.maximum)}°</span>
-      </div>
-      <div>
       <span class="weather-forecast-temp-min>${Math.round(day.temperature.minimum)}°</span>
       </div>
       </div>
